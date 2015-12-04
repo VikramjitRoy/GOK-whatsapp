ea374c5562b988ef59a2c0a0a1ccc6e730e91552
Vikramjit Roy <vikram.jee2012@gmail.com>
2015-12-04 23:48:49 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,35 @@
+#yowsup-cli demos --yowsup --login 919886864588:JwcHCoGbOsp7QcN5m7Qtc+aVxFU=
+from layer import EchoLayer
+from yowsup.layers.auth                        import YowAuthenticationProtocolLayer
+from yowsup.layers.protocol_messages           import YowMessagesProtocolLayer
+from yowsup.layers.protocol_receipts           import YowReceiptProtocolLayer
+from yowsup.layers.protocol_acks               import YowAckProtocolLayer
+from yowsup.layers.network                     import YowNetworkLayer
+from yowsup.layers.coder                       import YowCoderLayer
+from yowsup.stacks import YowStack
+from yowsup.common import YowConstants
+from yowsup.layers import YowLayerEvent
+from yowsup.stacks import YowStack, YOWSUP_CORE_LAYERS
+from yowsup import env
+from yowsup.layers.axolotl                     import YowAxolotlLayer
+from yowsup.layers.protocol_iq import YowIqProtocolLayer
+from yowsup.layers.protocol_media       import YowMediaProtocolLayer                                                                                   
+from yowsup.layers.protocol_media.mediauploader import MediaUploader
+
+CREDENTIALS = ("phone", "password") # replace with your phone and password
+if __name__==  "__main__":
+    layers = (
+        EchoLayer,
+        (YowAuthenticationProtocolLayer, YowMessagesProtocolLayer,YowMediaProtocolLayer, YowIqProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer),     
+        YowAxolotlLayer
+        )  + YOWSUP_CORE_LAYERS
+
+    stack = YowStack(layers) 
+    stack.setProp(YowAuthenticationProtocolLayer.PROP_CREDENTIALS, CREDENTIALS)         #setting credentials
+    stack.setProp(YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0])    #whatsapp server address
+    stack.setProp(YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN)      
+    stack.setProp(YowCoderLayer.PROP_RESOURCE, env.CURRENT_ENV.getResource())          #info about us as WhatsApp client
+    
+    stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))   #sending the connect signal
+    
+    stack.loop() #this is the program mainloop
